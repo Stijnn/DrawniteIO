726674553d9c871c08eebbfc48c3e628aa9679f7
Arno Nagtzaam <arno32@live.nl>
2019-09-24 15:17:22 +0200

    ewa 2e lobby push
    ja

--- DrawniteIO/DrawniteCore/Networking/Data/Lobby.cs
+++ DrawniteIO/DrawniteCore/Networking/Data/Lobby.cs
@@ -6,9 +6,27 @@ namespace DrawniteCore.Networking.Data
 {
     class Lobby
     {
+        private IList<ClientServer> connectedClients;
+        private readonly int port;
+        private Guid lobbyId;
+        private ClientServer lobbyLeader;
+        private LobbyStatus status;
 
-
-
-
+        public Lobby(int port, Guid lobbyId, ref ClientServer lobbyLeader)
+        {
+            this.port = port;
+            this.lobbyId = lobbyId;
+            this.lobbyLeader = lobbyLeader;
+            this.connectedClients = new List<ClientServer>();
+            this.connectedClients.Add(lobbyLeader);
+            status = LobbyStatus.AWAITING_START;
+        }
+    }
+    
+    enum LobbyStatus
+    {
+        AWAITING_START,
+        PLAYING,
+        AWAITING_RESTART,
     }
 }
